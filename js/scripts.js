7c6496dfb914a596b64cc920fbcf1823e4e6e519
Stephen Trewick <stephent818@gmail.com>
2019-03-29 12:07:25 -0700

    add initial javascript function with set variables, css to display:none html survey results

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#submit").click(function() {
+    var brain = $("input:radio[name=hemisphere]:checked").val();
+    var animal = $("input:radio[name=animal]:checked").val();
+    var situation = $("input:radio[name=problemSolve]:checked").val();
+    var dream = $("#future").val();
+    var freetime = $("#weekends").val();
+
+    if ()
+  });
+});
