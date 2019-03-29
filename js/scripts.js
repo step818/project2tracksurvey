a05851bcf03322fff9a9e46d48f1da4cdccebae9
Stephen Trewick <stephent818@gmail.com>
2019-03-29 13:58:35 -0700

    add if/else branches for multiple choice selected answers

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function(event) {
   $("#submit").click(function() {
     var brain = $("input:radio[name=hemisphere]:checked").val();
     var animal = $("input:radio[name=animal]:checked").val();
@@ -6,6 +6,39 @@ $(document).ready(function() {
     var dream = $("#future").val();
     var freetime = $("#weekends").val();
 
-    if ()
+    if ((brain === 'left' || brain === 'both') && (animal === 'cat' || animal === 'neither')) {
+      if ((situation === 'invincible' || situation === 'help') && (dream === 'extinction' || dream === 'space') && (freetime === 'hermit')) {
+        $("#ruby").show();
+        $("#java").hide();
+        $("#php").hide();
+        $("#intro").hide();
+      } else {
+
+      }
+    } else if ((brain === "both" || brain ==="right") && (animal === "dog" || animal === "cat")) {
+      if ((situation === "nap") && (dream === "harmony" || dream === "present") && (freetime === "nature" || freetime === "hermit")) {
+        $("#java").show();
+        $("#ruby").hide();
+        $("#php").hide();
+        $("#intro").hide();
+      } else {
+
+      }
+    } else if ((brain === "both") && (animal === "cat" || animal === "dog" || animal === "neither")) {
+      if ((situation === "help") && (dream === "space" || dream === "harmony" || dream === "extinction") && (freetime === "social" || freetime === "nature")) {
+        $("#php").show();
+        $("#ruby").hide();
+        $("#java").hide();
+        $("#intro").hide();
+      } else {
+
+      }
+    } else {
+      $("#intro").show();
+      $("#ruby").hide();
+      $("#java").hide();
+      $("#php").hide();
+    }
   });
+  event.preventDefault();
 });
