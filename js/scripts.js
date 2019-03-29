a7ecbc88fc805bf111b73440e278c2fe859c503a
Stephen Trewick <stephent818@gmail.com>
2019-03-29 15:11:36 -0700

    show css colors and fonts. also wells to radio forms

--- js/scripts.js
+++ js/scripts.js
@@ -6,39 +6,51 @@ $(document).ready(function() {
     var dream = $("#future").val();
     var freetime = $("#weekends").val();
 
-    if ((brain === 'left' || brain === 'both') && (animal === 'cat' || animal === 'neither')) {
-      if ((situation === 'invincible' || situation === 'help') && (dream === 'extinction' || dream === 'space') && (freetime === 'hermit')) {
-        $("#display").text("Ruby");
-        $("#ruby").show();
-        $("#java").hide();
-        $("#php").hide();
-        $("#intro").hide();
-      } else {
-
-      }
-    } else if ((brain === "both" || brain ==="right") && (animal === "dog" || animal === "cat")) {
-      if ((situation === "nap") && (dream === "harmony" || dream === "present") && (freetime === "nature" || freetime === "hermit")) {
-        $("#java").show();
-        $("#ruby").hide();
-        $("#php").hide();
-        $("#intro").hide();
-      } else {
-
-      }
-    } else if ((brain === "both") && (animal === "cat" || animal === "dog" || animal === "neither")) {
-      if ((situation === "help") && (dream === "space" || dream === "harmony" || dream === "extinction") && (freetime === "social" || freetime === "nature")) {
-        $("#php").show();
-        $("#ruby").hide();
-        $("#java").hide();
-        $("#intro").hide();
-      } else {
-
-      }
+    if ((dream === "default") || (freetime == "default")) {
+      alert("Please select an answer for every question.");
     } else {
-      $("#intro").show();
-      $("#ruby").hide();
-      $("#java").hide();
-      $("#php").hide();
+      if ((brain === 'left' || brain === 'both') && (animal === 'cat' || animal === 'neither')) {
+        if ((situation === 'invincible' || situation === 'help') && (dream === 'extinction' || dream === 'space') && (freetime === 'hermit')) {
+          $("#ruby").show();
+          $("#java").hide();
+          $("#php").hide();
+          $("#intro").hide();
+        } else {
+            $("#java").show();
+            $("#ruby").hide();
+            $("#php").hide();
+            $("#intro").hide();
+        }
+      } else if ((brain === "both" || brain ==="right") && (animal === "dog" || animal === "cat")) {
+        if ((situation === "nap") && (dream === "harmony" || dream === "present") && (freetime === "nature" || freetime === "hermit")) {
+          $("#java").show();
+          $("#ruby").hide();
+          $("#php").hide();
+          $("#intro").hide();
+        } else {
+            $("#php").show();
+            $("#ruby").hide();
+            $("#java").hide();
+            $("#intro").hide();
+        }
+      } else if ((brain === "both") && (animal === "cat" || animal === "dog" || animal === "neither")) {
+        if ((situation === "help") && (dream === "space" || dream === "harmony" || dream === "extinction") && (freetime === "social" || freetime === "nature")) {
+          $("#php").show();
+          $("#ruby").hide();
+          $("#java").hide();
+          $("#intro").hide();
+        } else {
+            $("#ruby").show();
+            $("#java").hide();
+            $("#php").hide();
+            $("#intro").hide();
+        }
+      } else {
+          $("#intro").show();
+          $("#ruby").hide();
+          $("#java").hide();
+          $("#php").hide();
+      }
     }
   });
 });
